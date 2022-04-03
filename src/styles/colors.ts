4f82cdf1c27f6570373bee28d403b898f6555279
Armando Joergensen <armando-a-j@hotmail.com>
2022-04-02 22:55:56 -0300

    Create default theme

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const appColors = {
+  brand01: "#00e7f9",
+  neutralDarkest: "#000000",
+  neutralMedium: "#8a8a8a",
+  neutralDark: "#202227",
+  neutralLightest: "#ffffff",
+  errorDarkest: "#ff0000",
+}
+
+export default appColors;
