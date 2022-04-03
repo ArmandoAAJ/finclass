48f605653d99528cd1f05bd144c06b88e169bb8b
Armando Joergensen <armando-a-j@hotmail.com>
2022-04-03 19:15:37 -0300

    Create navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import Button from "./components/Button";
-import Text from "./components/Text";
-import Separator from "./components/Separator/index";
-import BackButton from "./components/BackButton";
-import Dots from "./components/Dots";
-import Input from "./components/Input/Index";
-
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./navigation";
 const App = () => {
   return (
     <>
-      <Separator width={0} height={100} />
-      <Input error="ghf" rightIcon leftIcon secureTextEntry name="Senha" />
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     </>
   );
 };
---
+++ src/navigation/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignInStack from "./SignInStack";
+
+// import { Container } from './styles';
+
+const Navigation: React.FC = () => {
+  return <SignInStack />;
+};
+
+export default Navigation;
