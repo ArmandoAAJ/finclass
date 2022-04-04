d076d2b1e9c8e468784df795416d4a680af6c3fe
Armando Joergensen <armando-a-j@hotmail.com>
2022-04-03 21:28:57 -0300

    Create Login page and validation schema

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,109 @@
 import React from "react";
-import { View } from "react-native";
+import BackButton from "../../components/BackButton";
+import Separator from "../../components/Separator";
+import Text from "../../components/Text";
+import ImageLock from "../../../assets/icons/lock/icon.png";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Header,
+  LockIcon,
+  OptionRightHeader,
+} from "./styles";
+import { Controller, useForm } from "react-hook-form";
+import Input from "../../components/Input/Index";
+import Button from "../../components/Button";
+import { schemaLogin } from "./validation";
+import { useNavigation } from "@react-navigation/native";
 
 const Login: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schemaLogin),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  async function onSubmit() {
+    await handleSubmit(({ email, password }) => {
+      //TODO
+    })();
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Separator height={20} />
+        <Header>
+          <BackButton onPress={handleGoBack} />
+          <OptionRightHeader>
+            <Text size={13}>Recuperar senha</Text>
+            <Separator width={10} />
+            <LockIcon source={ImageLock} />
+          </OptionRightHeader>
+        </Header>
+        <Separator height={30} />
+        <Text size={20} type="semiBold">
+          Acessar minha conta
+        </Text>
+        <Separator height={30} />
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => {
+            return (
+              <Input
+                autoCompleteType="email"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="E-mail"
+                placeholder="Insira seu e-mail"
+                onChange={onChange}
+                onChangeText={(text) => setValue("email", text)}
+                onBlur={onBlur}
+                value={value}
+                error={errors.email?.message}
+              />
+            );
+          }}
+        />
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, onBlur, value } }) => {
+            return (
+              <Input
+                autoCompleteType="password"
+                autoCapitalize="none"
+                name="Senha"
+                placeholder="Insira sua senha"
+                onChange={onChange}
+                onChangeText={(text) => setValue("password", text)}
+                onBlur={onBlur}
+                value={value}
+                error={errors.password?.message}
+              />
+            );
+          }}
+        />
+        <Separator height={55} />
+        <Button title="Entrar" onPress={onSubmit} textType="semiBold" />
+      </Content>
+    </Container>
+  );
 };
 
 export default Login;
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled.View``;
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background.main};
+`;
+
+export const Content = styled.View`
+  margin: 0 20px;
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const OptionRightHeader = styled.View`
+  flex-direction: row;
+`;
+
+export const LockIcon = styled.Image.attrs({
+  resizeMode: "contain",
+})`
+  height: 16px;
+  width: 16px;
+`;
