b6285646e25e5248c369283926cbdda4c51a8465
Armando Joergensen <armando-a-j@hotmail.com>
2022-04-03 21:27:49 -0300

    Remove SafeArea component

--- src/components/SafeArea/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ContainerProps } from "./types";
-
-export const Container = styled(SafeAreaView)<ContainerProps>`
-  flex: 1;
-  background-color: ${({ theme, color }) =>
-    color ? color : theme.colors.background.main};
-`;
--- src/components/SafeArea/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Props {
-  color?: string;
-}
-
-export interface ContainerProps {
-  color?: string;
-}
