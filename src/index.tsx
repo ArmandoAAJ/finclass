22070f71b72077f3476c633f8ca1a7e7ef66d076
Armando Joergensen <armando-a-j@hotmail.com>
2022-04-04 20:34:23 -0300

    Update navigators

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./navigation";
+import AuthProvider from "./context/Auth";
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <StatusBar style="light" />
       <NavigationContainer>
         <Navigation />
       </NavigationContainer>
-    </>
+    </AuthProvider>
   );
 };
 
